bbb8488e2fd5378ffa9b4640d90965d19e3c024f
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-07-02 01:44:47 +0200

    Change the ValidGenre Linq MethodAdded POST Movie End Point with handling

--- Models/CustomResponse.cs
+++ Models/CustomResponse.cs
@@ -8,5 +8,7 @@
 		public string Message { get; set; }
 
 		public T Data { get; set; }
+
+		public List<string>? Errors { get; set; }
 	}
 }
---
+++ Models/MovieRequestDto.cs
@@ -0,0 +1,21 @@
+﻿namespace MoviesApi.Models
+{
+	public record MovieRequestDto
+	{
+		[Required]
+		[MaxLength(250)]
+		public string Title { get; set; }
+
+		public int? Year { get; set; }
+
+		[Precision(2,1)]
+		public double? Rate { get; set; }
+
+		[MaxLength(2500)]
+		public string? StoryLine { get; set; }
+
+		public IFormFile? Poster { get; set; }
+
+		public byte? GenreID { get; set; }
+	}
+}
